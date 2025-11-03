66a790a065dbb6a0d6d3d0bd700d4f0827929387
malawllaqi <malawllaqi@gmail.com>
2025-11-03 20:53:09 +0300

    setup auth

--- apps/api/src/utils/server.ts
+++ apps/api/src/utils/server.ts
@@ -1,25 +1,55 @@
+import fastifyCookie from "@fastify/cookie";
+import { auth } from "@tawasull/auth";
 import type { DB } from "@tawasull/db";
-import Fastify from "fastify";
+import Fastify, { type FastifyReply, type FastifyRequest } from "fastify";
+import { authRouter } from "@/modules/auth/auth.route";
 import { getEnv } from "./env";
 import { envToLogger } from "./logger";
+import type { Session, User } from "./types";
 
 declare module "fastify" {
 	interface FastifyRequest {
 		db: DB;
+		session: Session;
+		user: User;
+	}
+
+	interface FastifyInstance {
+		authenticate: typeof authenticate;
 	}
 }
 
+async function authenticate(req: FastifyRequest, reply: FastifyReply) {
+	if (!req.user) {
+		reply.code(401).send({ message: "Unauthorized" });
+	}
+}
 export async function buildServer(db: DB) {
-	const environment = getEnv("NODE_ENV");
 	const fastify = Fastify({
-		logger: envToLogger[environment],
+		logger: envToLogger[getEnv("NODE_ENV")],
 	});
+
+	fastify.register(fastifyCookie);
 
 	fastify.addHook("onRequest", async (req) => {
 		req.db = db;
 	});
 
+	fastify.addHook("onRequest", async (req) => {
+		const sessionData = await auth.api.getSession({
+			headers: new Headers(req.headers as Record<string, string>),
+		});
+
+		if (sessionData) {
+			req.session = sessionData.session;
+			req.user = sessionData.user;
+		}
+	});
+
+	fastify.decorate("authenticate", authenticate);
+
 	fastify.after(() => {
+		fastify.register(authRouter);
 		fastify.get("/healthcheck", async () => {
 			return { status: "ok" };
 		});
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,18 +2,22 @@ import { setupDB } from "@tawasull/db";
 import * as schema from "@tawasull/db/schema/auth";
 import { type BetterAuthOptions, betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { openAPI, username } from "better-auth/plugins";
+
+const db = (await setupDB()).db;
 
 export const auth = betterAuth<BetterAuthOptions>({
-	database: drizzleAdapter(setupDB(), {
+	database: drizzleAdapter(db, {
 		provider: "pg",
-
 		schema: schema,
 	}),
 	trustedOrigins: [process.env.CORS_ORIGIN || ""],
 	emailAndPassword: {
 		enabled: true,
 	},
+	plugins: [username(), openAPI()],
 	advanced: {
+		disableOriginCheck: true,
 		defaultCookieAttributes: {
 			sameSite: "none",
 			secure: true,
