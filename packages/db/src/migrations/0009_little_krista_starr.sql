5bb1b4652b72df5326e4aeee3551fa770d72fdd8
malawllaqi <malawllaqi@gmail.com>
2025-12-03 08:07:05 +0300

    fix: db migration

--- packages/db/src/migrations/0009_little_krista_starr.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "comment" ALTER COLUMN "user_id" SET DATA TYPE text;
--- packages/db/src/migrations/0012_zippy_joseph.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "notification" ALTER COLUMN "comment_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "notification" ALTER COLUMN "post_id" SET DATA TYPE uuid;
